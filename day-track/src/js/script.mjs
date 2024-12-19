d9fc599d98d711f6563c441950e7ca37ab97e7bb
Sana Khuram <sana.khuram.baig@gmail.com>
2024-12-19 10:08:38 +0100

    Add theme button functionality
    -feat: add theme button functionality to switch between day, evening, and night modes
    -feat: implement default theme setting based on current time

--- day-track/src/js/script.mjs
+++ day-track/src/js/script.mjs
@@ -1,4 +1,3 @@
-// Select buttons
 const dayButton = document.getElementById('dayButton');
 const eveningButton = document.getElementById('eveningButton');
 const nightButton = document.getElementById('nightButton');
@@ -24,4 +23,33 @@ function setDefaultTheme() {
     }
 }
 
-window.addEventListener('DOMContentLoaded', setDefaultTheme);
+function generateStars() {
+    const numberOfStars = 100;
+    const starsContainer = document.querySelector('.stars');
+
+    for (let i = 0; i < numberOfStars; i++) {
+        const star = document.createElement('div');
+        star.classList.add('star');
+
+        const top = getRandomValue(0, 100);
+        const left = getRandomValue(0, 100);
+        star.style.top = `${top}%`;
+        star.style.left = `${left}%`;
+
+        const delay = getRandomValue(0, 5);
+        const duration = getRandomValue(2, 4);
+        star.style.animationDelay = `${delay}s`;
+        star.style.animationDuration = `${duration}s`;
+
+        starsContainer.appendChild(star);
+    }
+}
+
+function getRandomValue(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    setDefaultTheme();
+    generateStars();
+});
